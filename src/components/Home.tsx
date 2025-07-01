99ad66f32af2e445716019be1e198d889de98a97
Mccullahz <zylardmccullah@gmail.com>
2025-07-01 15:33:36 -0400

    Updates

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/App.css';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <div className= "Body">
+	<p>Welcome to my development log! Here, I will share the inner working and thought processes of my active projects. Hopefully this will serve as reference material for myself, as well as others </p>
+	<p>Stay tuned for updates!</p>
+      </div>
+     </div>
+  );
+}
