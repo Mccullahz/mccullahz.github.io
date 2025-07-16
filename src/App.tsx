f73bfca8c36e39feb4974664d89fc00809b334a3
Mccullahz <zylardmccullah@gmail.com>
2025-07-16 18:49:03 -0400

    updated styles and navbar so we are now all static, makes things work smoother in gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import './styles/App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
 return (
 <div className="App">
 		<Navbar />
-		<Header />
 		<Home />
 		<Footer />
 </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,25 @@
 import React, { useEffect } from "react";
 import "../styles/App.css";
-{/* this is kind of a gross way of going about the navbar, BUT i am pretty sure this will make this functional in gh-pages*/} 
+{/* changed this so now we are a static navbar, dont need anything super fancy here*/} 
+
 export default function Navbar() {
-  useEffect(() => {
-    const toggleBtn = document.querySelector("[data-nav-toggler]") as HTMLElement | null;
-    const navbar = document.querySelector("[data-navbar]") as HTMLElement | null;
-
-    if (toggleBtn && navbar) {
-      const handleClick = () => {
-        toggleBtn.classList.toggle("active");
-        navbar.classList.toggle("active");
-      };
-      toggleBtn.addEventListener("click", handleClick);
-      return () => toggleBtn.removeEventListener("click", handleClick);
-    }
-  }, []);
-
   return (
-    <nav className="navbar" data-navbar>
-      <button className="nav-toggle" data-nav-toggler aria-label="Toggle navigation">
-        ☰
-      </button>
-      <ul className="nav-links">
-        <li><a href="#">Projects</a></li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <span>📁</span>
+        <span style={{ marginLeft: "0.5rem" }}>Mccullahz / DevLog</span>
+      </div>
+      <div className="navbar-links">
+        <a className="navbar-link" href="#">1</a>
+        <a className="navbar-link" href="#">2</a>
+        <a className="navbar-link" href="#">3</a>
+        <a className="navbar-link" href="#">4</a>
+        <a className="navbar-link" href="#">5</a>
+        <a className="navbar-link" href="#">6</a>
+        <a className="navbar-link" href="#">7</a>
+        <a className="navbar-link" href="#">8</a>
+        <a className="navbar-link" href="#">Settings</a>
+      </div>
     </nav>
   );
 }
